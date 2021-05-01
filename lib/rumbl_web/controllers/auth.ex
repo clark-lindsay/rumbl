d05557cec5de3d0df4b5c375d71bf68e76625669
clark-lindsay <clarklindsay96@gmail.com>
2021-05-01 16:18:28 -0400

    Fix indentation mistake

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -23,7 +23,7 @@ defmodule RumblWeb.Auth do
       |> configure_session(drop: true)
   end
 
-def authenticate_user(conn, _opts) do
+  def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
